ed428eb740486e048fd8fc5d42007fd841c825d4
Z.Y <catherineyung1992@gmail.com>
2021-12-10 11:21:58 +0100

    Google map v2.0 : return location

--- app/src/main/java/com/project/plantappui/MainActivity.java
+++ app/src/main/java/com/project/plantappui/MainActivity.java
@@ -1,16 +1,23 @@
 package com.project.plantappui;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -23,6 +30,9 @@ import com.project.plantappui.menu.camera.CameraFragment;
 import com.project.plantappui.menu.map.MapFragment;
 import com.project.plantappui.menu.home.HomeFragment;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import nl.joery.animatedbottombar.AnimatedBottomBar;
@@ -33,6 +43,16 @@ public class MainActivity extends AppCompatActivity {
     AnimatedBottomBar animatedBottomBar;
     FragmentManager fragmentManager;
 
+    /* for location */
+    // store longitude and latitude
+    Map<String, Double> locationMap = new HashMap<String, Double>(){
+        {put("longitude", -1.0);
+        put("latitude", -1.0);}
+    };
+    private LocationManager locationManager;
+    private String provider;// position provider
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +61,11 @@ public class MainActivity extends AppCompatActivity {
         setToolbar();
         initViews(savedInstanceState);
         initComponentsNavHeader();
+
+        getLocation();
+        // test: show position
+        Toast.makeText(getApplicationContext(), locationMap.get("longitude")+","+locationMap.get("latitude"),Toast.LENGTH_LONG).show();
+
     }
 
     private void setToolbar() {
@@ -108,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
         toggle.syncState();
     }
 
-    private void initComponentsNavHeader(){
+    private void initComponentsNavHeader() {
         NavigationView navigationView = findViewById(R.id.nav_view);
 //        navigationView.setItemIconTintList(null); //disable tint on each icon to use color icon svg
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -149,4 +174,74 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+    /*----------- Location relevant - start --------------*/
+    public void getLocation() {
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // update current location
+
+        // Get all available position provider
+        List<String> providerList = locationManager.getProviders(true);
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            // no position provider available
+            Toast.makeText(MainActivity.this, "no position provider available", Toast.LENGTH_LONG).show();
+            locationMap.put("longitude", -1.0);
+            locationMap.put("latitude", -1.0);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return ;
+        }
+        locationManager.requestLocationUpdates(provider, 10 * 1000, 1,locationListener);
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            // show current location
+            locationMap.put("longitude", location.getLongitude());
+            locationMap.put("latitude", location.getLatitude());
+        }
+    }
+
+
+    LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onLocationChanged(Location location) {
+            locationMap.put("longitude", location.getLongitude());
+            locationMap.put("latitude", location.getLatitude());
+        }
+    };
+    /*----------- Location relevant - end --------------*/
+
+    protected void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            locationManager.removeUpdates(locationListener);
+        }
+    };
+
+
 }
